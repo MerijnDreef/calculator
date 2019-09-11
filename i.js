3491438e88acfe0d23db5b9ab02a48de32a4a24e
Merijn <99054535@mydavinci.nl>
2019-09-11 11:52:44 +0200

    calculator
    hulp voor calculator

---
+++ i.js
@@ -0,0 +1,19 @@
+var i = 1;
+var tafel = prompt("tafel?");
+for (i == 1; i < 11; i++) {
+    document.write(tafel+"x"+i+"="+(tafel*i)+"<br>"); 
+}
+//var cijfer = 12;
+//cijfer += 6
+//cijfer *= 10
+//cijfer /= 5
+//cijfer -= 12
+//document.write(cijfer+"<br>");
+
+var getal = prompt("calculator voor alles");
+getal += 6
+getal *= 10
+getal /= 5
+getal -= 12
+getal /= 10
+document.write(getal);
